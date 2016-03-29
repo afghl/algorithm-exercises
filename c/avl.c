fb937571630d4204605d8fcab7c63e58883e059c
afghl <airport.0930@gmail.com>
2016-03-29 21:39:47 +0800

    avl append node

---
+++ c/avl.c
@@ -0,0 +1,70 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+# define FALSE 0
+# define TRUE 1
+
+typedef int ElementType;
+
+// avl
+
+typedef struct TreeNode {
+  ElementType element;
+  int bf; // 平衡因子
+  struct TreeNode * left;
+  struct TreeNode * right;
+}TreeNode;
+typedef TreeNode *TreePtr;
+
+TreePtr createTree(ElementType element){
+	TreePtr tree = (TreePtr)malloc(sizeof(struct TreeNode));
+  tree->element = element;
+  tree->left = NULL;
+  tree->right = NULL;
+	return tree;
+}
+
+void appendNode(TreePtr tree, ElementType element) {
+  if(tree->element == element) {
+    printf("existed\n");
+    return;
+  }
+  if(element < tree->element && tree->left != NULL) {
+    return appendNode(tree->left, element);
+  }
+  if(element > tree->element && tree->right != NULL) {
+    return appendNode(tree->right, element);
+  }
+
+  TreePtr node = (TreePtr)malloc(sizeof(struct TreeNode));
+  node->element = element;
+  node->left = NULL;
+  node->right = NULL;
+  if(element < tree->element) {
+    tree->left = node;
+  }
+  if(element > tree->element) {
+    tree->right = node;
+  }
+}
+
+void inOrderTraversal(TreePtr tree) {
+	if(tree) {
+		inOrderTraversal(tree->left);
+		printf("%d ", tree->element);
+		inOrderTraversal(tree->right);
+	}
+}
+
+int main(){
+  TreePtr tree = createTree(5);
+  appendNode(tree, 4);
+  appendNode(tree, 7);
+  appendNode(tree, 6);
+  appendNode(tree, 1);
+  appendNode(tree, 2);
+  appendNode(tree, 3);  
+
+  inOrderTraversal(tree);
+
+}
