2627d37ee9e7d156261f382cd4bee1c39ce67b24
afghl <airport.0930@gmail.com>
2016-09-02 21:19:27 +0800

    add test

--- c/sequence_list/main.c
+++ c/sequence_list/main.c
@@ -11,28 +11,32 @@ int main()
     int N;
 
     L = MakeEmpty();
-    scanf("%d", &N);
-    while ( N-- ) {
-        scanf("%d", &X);
-        if ( Insert(L, X, 0)==false )
-            printf(" Insertion Error: %d is not in.\n", X);
-    }
-    scanf("%d", &N);
-    while ( N-- ) {
-        scanf("%d", &X);
-        P = Find(L, X);
-        if ( P == ERROR )
-            printf("Finding Error: %d is not in.\n", X);
-        else
-            printf("%d is at position %d.\n", X, P);
-    }
-    scanf("%d", &N);
-    while ( N-- ) {
-        scanf("%d", &P);
-        if ( Delete(L, P)==false )
-            printf(" Deletion Error.\n");
-        if ( Insert(L, 0, P)==false )
-            printf(" Insertion Error: 0 is not in.\n");
-    }
-    return 0;
+
+    // test Insert
+    Insert(L, 1, 0);
+    Insert(L, 2, 0);
+    Insert(L, 3, 0);
+    assert(L->Data[2] == 1);
+    assert(L->Data[1] == 2);
+    assert(L->Last == 2);
+
+    Insert(L, 4, 2);
+    assert(L->Data[2] == 4);
+    assert(L->Data[3] == 1);
+
+    // test insert error
+    assert(Insert(L, 5, 13) == false);
+
+    // test find
+
+    assert(Find(L, 4) == 2);
+    assert(Find(L, 5) == ERROR);
+
+    // test delete
+    assert(Delete(L, 7) == false);
+    assert(Delete(L, 1) == true);
+
+    assert(L->Last == 2);
+    assert(Find(L, 1) == ERROR);
+    printf("\npass!\n");
 }
