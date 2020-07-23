d33aba75f90a024f8050ba889c2635aa489c9a05
afghl <airport.0930@gmail.com>
2020-07-23 09:59:49 +0800

    add methods

---
+++ leetcode/src/main/java/leetcode/第N个数字_400.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+/**
+ * https://leetcode-cn.com/problems/nth-digit/
+ *
+ * mid
+ */
+public class 第N个数字_400 {
+
+    // 有9个长度为1的数
+    // 90个长度为2的数
+    // 900个长度为3的数
+    // ...
+    public int findNthDigit(int n) {
+
+        // TODO: pending...
+        return -1;
+    }
+}
