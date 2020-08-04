9ad7a56dc5171804e69b83185327ea47c5b00aa0
chenjunyao <chenjunyao@yy.com>
2020-08-04 09:52:17 +0800

    add

---
+++ leetcode/src/main/java/leetcode/比较含退格的字符串_844.java
@@ -0,0 +1,43 @@
+package leetcode;
+
+import com.sun.corba.se.impl.orbutil.ObjectUtility;
+import com.sun.xml.internal.ws.util.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * https://leetcode-cn.com/problems/backspace-string-compare/
+ *
+ * easy
+ */
+public class 比较含退格的字符串_844 {
+
+    /**
+     * 普通做法：拿到转化后的str，用栈
+     * @param S
+     * @param T
+     * @return
+     */
+    public boolean backspaceCompare1(String S, String T) {
+        String realSForS = getRealStr(S);
+        String realSForT = getRealStr(T);
+        return realSForS.equals(realSForT);
+    }
+
+    private String getRealStr(String s) {
+        if (s == null || "".equals(s)) {
+            return "";
+        }
+        Stack<Character> ans = new Stack();
+        for (char c: s.toCharArray()) {
+            if (c != '#')
+                ans.push(c);
+            else if (!ans.empty())
+                ans.pop();
+        }
+
+        return String.valueOf(ans);
+    }
+}
