d6dd91b91c5294e99320c34d1dc85eb436a574cd
afghl <airport.0930@gmail.com>
2020-08-21 09:56:31 +0800

    comments

--- leetcode/src/main/java/leetcode/pending/第N个数字_400.java
+++ leetcode/src/main/java/leetcode/pending/第N个数字_400.java
@@ -5,12 +5,24 @@ package leetcode.pending;
  *
  * mid
  */
+/**
+ * 输入:
+ * 11
+ *
+ * 输出:
+ * 0
+ *
+ * 说明:
+ * 第11个数字在序列 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ... 里是0，它是10的一部分。
+ * 
+ */
 public class 第N个数字_400 {
 
     // 有9个长度为1的数
     // 90个长度为2的数
     // 900个长度为3的数
     // ...
+
     public int findNthDigit(int n) {
 
         // TODO: pending...
