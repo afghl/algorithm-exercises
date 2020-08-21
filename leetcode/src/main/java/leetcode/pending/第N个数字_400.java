a210f27d3d918a1d8b3785e6f9c12528fe37e613
afghl <airport.0930@gmail.com>
2020-08-21 10:10:44 +0800

    add pow methods

--- leetcode/src/main/java/leetcode/pending/第N个数字_400.java
+++ leetcode/src/main/java/leetcode/pending/第N个数字_400.java
@@ -14,9 +14,15 @@ package leetcode.pending;
  *
  * 说明:
  * 第11个数字在序列 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ... 里是0，它是10的一部分。
- * 
+ *
  */
 public class 第N个数字_400 {
+
+    public static void main(String[] args) {
+        new 第N个数字_400().findNthDigit(9);
+        new 第N个数字_400().findNthDigit(100);
+        new 第N个数字_400().findNthDigit(1000);
+    }
 
     // 有9个长度为1的数
     // 90个长度为2的数
@@ -25,7 +31,21 @@ public class 第N个数字_400 {
 
     public int findNthDigit(int n) {
 
-        // TODO: pending...
+        int weishu = 1;
+
+        int remain = n;
+
+        while (remain > 0) {
+            remain = n - 9 * pow(weishu);
+            weishu += 1;
+        }
+
+        System.out.println(String.format("remain: %s, weishu: %s", remain, weishu));
+
         return -1;
+    }
+
+    private int pow(int weishu) {
+        return (int) Math.pow(10, weishu -1);
     }
 }
