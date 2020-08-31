cf425c8046c0177073d9ef030d4d059785ba5e2d
afghl <airport.0930@gmail.com>
2020-08-31 11:22:54 +0800

    add.

--- leetcode/src/main/java/leetcode/pending/第N个数字_400.java
+++ leetcode/src/main/java/leetcode/pending/第N个数字_400.java
@@ -19,8 +19,8 @@ package leetcode.pending;
 public class 第N个数字_400 {
 
     public static void main(String[] args) {
-        new 第N个数字_400().findNthDigit(9);
-        new 第N个数字_400().findNthDigit(100);
+        new 第N个数字_400().findNthDigit(6);
+        new 第N个数字_400().findNthDigit(44);
         new 第N个数字_400().findNthDigit(1000);
     }
 
@@ -30,22 +30,25 @@ public class 第N个数字_400 {
     // ...
 
     public int findNthDigit(int n) {
-
-        int weishu = 1;
-
+        int weishu = 0;
         int remain = n;
 
-        while (remain > 0) {
-            remain = n - 9 * pow(weishu);
+        while (remain - 9 * pow(weishu) > 0) {
+            remain = remain - 9 * pow(weishu);
             weishu += 1;
         }
 
         System.out.println(String.format("remain: %s, weishu: %s", remain, weishu));
 
+        // 算出来有在这个位数下的游标了，再找找这个游标前，一共有几个数
+        int count = remain / (weishu + 1);
+        System.out.println("count= "+count);
+
+
         return -1;
     }
 
     private int pow(int weishu) {
-        return (int) Math.pow(10, weishu -1);
+        return (int) Math.pow(10, weishu);
     }
 }
---
+++ leetcode/src/main/java/leetcode/好数对的数目_1512.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+/**
+ * https://leetcode-cn.com/problems/number-of-good-pairs/
+ *
+ * easy
+ *
+ */
+public class 好数对的数目_1512 {
+
+    /**
+     * 给你一个整数数组 nums 。
+     *
+     * 如果一组数字 (i,j) 满足 nums[i] == nums[j] 且 i < j ，就可以认为这是一组 好数对 。
+     *
+     * 返回好数对的数目。
+     *
+     */
+    public int numIdenticalPairs(int[] nums) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
