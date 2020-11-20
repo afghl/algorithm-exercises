90ace3406e364ab6da7d237333b45a234a0a3445
afghl <airport.0930@gmail.com>
2020-11-20 11:52:49 +0800

    add

--- leetcode/src/main/java/leetcode/array/最大子序和_53.java
+++ leetcode/src/main/java/leetcode/array/最大子序和_53.java
@@ -15,13 +15,17 @@ package leetcode.array;
 public class 最大子序和_53 {
 
     public static void main(String[] args) {
-        new 最大子序和_53().maxSubArray(new int[] {-2,1,-3,4,-1,2,1,-5,4});
+        new 最大子序和_53().maxSubArray(new int[] {-3, 1,1,-5,1,4,5});
     }
     public int maxSubArray(int[] nums) {
         int pre = 0, maxAns = nums[0];
 
         // 这里pre是上一个指针所指向的位，以它为结尾的子序和的最大值
         for (int x : nums) {
+            // 这里pre是上一个指针所指向的位，以它为结尾的子序和的最大值
+            // x是当前的位
+            // 意思是如果x加入使之前的结果（pre）> 只以x作为子序列的结果，那么将这个x纳入之前的子序列里
+            // 否则，直接以当前x作为一个新的子序列的开始（则将x赋值给pre）
             pre = Math.max(pre + x, x);
             maxAns = Math.max(maxAns, pre);
         }
---
+++ leetcode/src/main/java/leetcode/pending/下一个排列_31.java
@@ -0,0 +1,16 @@
+package leetcode.pending;
+
+/**
+ * https://leetcode-cn.com/problems/next-permutation/
+ */
+public class 下一个排列_31 {
+
+    public void nextPermutation(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+
+        int p = 0, q = 1;
+//        while ()
+    }
+}
