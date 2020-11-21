fcb44cc3ac148912153ed7a41b22f8d8b4ab2172
afghl <airport.0930@gmail.com>
2020-11-21 18:17:27 +0800

    add reverse linked list

---
+++ leetcode/src/main/java/leetcode/linkedlist/反转链表_206.java
@@ -0,0 +1,43 @@
+package leetcode.linkedlist;
+
+/**
+ * https://leetcode-cn.com/problems/reverse-linked-list/
+ *
+ * easy
+ */
+public class 反转链表_206 {
+
+    /**
+     * 反转一个单链表。
+     *
+     * 示例:
+     *
+     * 输入: 1->2->3->4->5->NULL
+     * 输出: 5->4->3->2->1->NULL
+     * 进阶:
+     * 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+     *
+     * @param head
+     * @return
+     */
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode next;
+
+        while (curr != null) {
+            next = curr.next; // 2
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+
+    //1->2->3->4->5->NULL
+}
