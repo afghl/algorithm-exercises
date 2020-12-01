e266b3aee3a6ad35ffe808f5bb0eaa21339976ba
afghl <airport.0930@gmail.com>
2020-12-01 12:01:34 +0800

    add IntersectionNode

---
+++ leetcode/src/main/java/leetcode/linkedlist/两个链表的第一个公共节点_offer52.java
@@ -0,0 +1,20 @@
+package leetcode.linkedlist;
+
+/**
+ * https://leetcode-cn.com/problems/liang-ge-lian-biao-de-di-yi-ge-gong-gong-jie-dian-lcof/
+ *
+ * easy
+ */
+public class 两个链表的第一个公共节点_offer52 {
+
+    /**
+     * 双指针
+     *
+     * @param headA
+     * @param headB
+     * @return
+     */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+
+    }
+}
