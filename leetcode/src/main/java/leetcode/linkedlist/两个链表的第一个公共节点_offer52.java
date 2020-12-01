7a7aeac3fa77f52f43721056991df0fba430041b
afghl <airport.0930@gmail.com>
2020-12-01 13:21:02 +0800

    add

--- leetcode/src/main/java/leetcode/linkedlist/两个链表的第一个公共节点_offer52.java
+++ leetcode/src/main/java/leetcode/linkedlist/两个链表的第一个公共节点_offer52.java
@@ -9,12 +9,24 @@ public class 两个链表的第一个公共节点_offer52 {
 
     /**
      * 双指针
+     * 假设listA和listB有交集于listC
+     * 则：
+     * len(listA) = A + C
+     * len(listB) = B + C
+     * 两个指针p + q分别遍历完A + C + B，则会相遇，且相遇在listC的起点
      *
      * @param headA
      * @param headB
      * @return
      */
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode p = headA, q = headB;
 
+        while (q != p) {
+            p = p == null ? headB : p.next;
+            q = q == null ? headA : q.next;
+        }
+
+        return q;
     }
 }
