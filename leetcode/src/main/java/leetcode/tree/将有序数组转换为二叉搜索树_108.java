987eae6a6bccb41fffb11085b3b4c5af52f8472c
afghl <airport.0930@gmail.com>
2020-12-10 14:00:18 +0800

    add one tree

--- leetcode/src/main/java/leetcode/tree/将有序数组转换为二叉搜索树_108.java
+++ leetcode/src/main/java/leetcode/tree/将有序数组转换为二叉搜索树_108.java
@@ -27,6 +27,27 @@ public class 将有序数组转换为二叉搜索树_108 {
      * @return
      */
     public TreeNode sortedArrayToBST(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
 
+        // 因为是升序的，所以利用这个特性
+        return buildTree(nums, 0, nums.length - 1);
     }
+
+    private TreeNode buildTree(int[] nums, int left, int right) {
+        if (left >= right) {
+            return null;
+        }
+
+        int mid = (left + right) / 2;
+
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = buildTree(nums, left, mid - 1);
+        node.right = buildTree(nums, mid + 1, right);
+
+        return node;
+    }
+
+
 }
