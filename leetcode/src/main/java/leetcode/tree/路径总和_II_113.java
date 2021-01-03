bf6002ad8735be7ba20dfe972f2e8a1c4416da9c
afghl <airport.0930@gmail.com>
2021-01-03 11:38:32 +0800

    simplify

--- leetcode/src/main/java/leetcode/tree/路径总和_II_113.java
+++ leetcode/src/main/java/leetcode/tree/路径总和_II_113.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 /**
  * https://leetcode-cn.com/problems/path-sum-ii/
- *
+ * <p>
  * medium
  */
 public class 路径总和_II_113 {
@@ -18,12 +18,16 @@ public class 路径总和_II_113 {
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> path = new ArrayList<>();
 
-        traversal(root, sum, 0, result, path);
+        dfs(root, sum, 0, result, path);
 
         return result;
     }
 
-    private void traversal(TreeNode node, int sum, int current, List<List<Integer>> result, List<Integer> path) {
+    private void dfs(TreeNode node, int sum, int current, List<List<Integer>> result, List<Integer> path) {
+        if (node == null) {
+            return;
+        }
+
         current += node.val;
         path.add(node.val);
 
@@ -32,14 +36,11 @@ public class 路径总和_II_113 {
             if (current == sum) {
                 result.add(path);
             }
+            return;
         }
 
         // 否则继续遍历
-        if (node.left != null) {
-            traversal(node.left, sum, current, result, new ArrayList<>(path));
-        }
-        if (node.right != null) {
-            traversal(node.right, sum, current, result, new ArrayList<>(path));
-        }
+        dfs(node.left, sum, current, result, new ArrayList<>(path));
+        dfs(node.right, sum, current, result, new ArrayList<>(path));
     }
 }
