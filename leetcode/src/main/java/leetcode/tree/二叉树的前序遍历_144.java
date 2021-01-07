bfc37bb25ab9d567286dc9621f9120d82de08cc5
afghl <airport.0930@gmail.com>
2021-01-07 18:14:21 +0800

    tree traversal

---
+++ leetcode/src/main/java/leetcode/tree/二叉树的前序遍历_144.java
@@ -0,0 +1,48 @@
+package leetcode.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * https://leetcode-cn.com/problems/binary-tree-preorder-traversal/
+ *
+ * medium
+ */
+public class 二叉树的前序遍历_144 {
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Stack<TreeNode> s = new Stack<>();
+        s.push(root);
+
+        while (!s.empty()) {
+            TreeNode node = s.pop();
+            result.add(node.val);
+            if (node.right != null) {
+                s.push(node.right);
+            }
+            if (node.left != null) {
+                s.push(node.left);
+            }
+        }
+
+        return result;
+    }
+
+    private void doTraversal(TreeNode node, List<Integer> result) {
+        if (node == null) {
+            return;
+        }
+
+        result.add(node.val);
+        doTraversal(node.left, result);
+        doTraversal(node.right, result);
+    }
+
+
+}
