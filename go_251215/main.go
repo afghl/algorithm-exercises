d0faed5e488f3f72457f35e594d31123f3d1e891
afghl <airport.0930@gmail.com>
2026-01-04 16:40:15 +0800

    f

--- go_251215/main.go
+++ go_251215/main.go
@@ -1,12 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
 	// l := 4,2,1,3
 
 	// sortList(getList([]int{4, 2, 1, 3}))
 	// jump([]int{1, 2})
 	// combine(4, 2)
-	hIndex([]int{0, 1, 3, 5, 6})
+	// hIndex([]int{0, 1, 3, 5, 6})
+	fmt.Printf("ans: %v\n", reverseWords("the sky is blue"))
 }
 func getList(arr []int) *ListNode {
 	head := &ListNode{}
--- go_251215/searchRange.go
+++ go_251215/searchRange.go
@@ -1,5 +1,29 @@
 package main
 
 func searchRange(nums []int, target int) []int {
-	return nil
+	if len(nums) == 0 {
+		return []int{-1, -1}
+
+	}
+	leftIdx := binSearchRange(nums, target, true)
+	rightIdx := binSearchRange(nums, target, false) - 1
+	if leftIdx <= rightIdx && rightIdx < len(nums) && nums[leftIdx] == target && nums[rightIdx] == target {
+		return []int{leftIdx, rightIdx}
+	}
+	return []int{-1, -1}
+}
+
+func binSearchRange(nums []int, target int, start bool) int {
+	l, r, ans := 0, len(nums)-1, len(nums)
+	for l <= r {
+		m := (l + r) / 2
+		num := nums[m]
+		if num > target || (start && num >= target) {
+			r = m - 1
+			ans = m
+		} else {
+			l = m + 1
+		}
+	}
+	return ans
 }
